33dae7767609c8df7c3810f4d47e5497c1415d13
David Neumann <86894039+davidneumann01@users.noreply.github.com>
2022-11-30 22:38:35 -0300

    Add files via upload

--- aplicacion.py
+++ aplicacion.py
@@ -9,28 +9,31 @@ from tkinter import messagebox as mb
 from urllib import request
 from urllib import error
 
+
 class Aplicacion:
-	def __init__(self):
-		self.ventana1=tk.Tk()
-		self.label1=ttk.Label(text="Ingrese URL del sitio web:")
-		self.label1.grid(column=0, row=0, pady=10)
-		self.dato1=tk.StringVar()
-		self.entry1=ttk.Entry(self.ventana1, width=100, textvariable=self.dato1)
-		self.entry1.grid(column=0, row=1, pady=10)
-		self.boton1=ttk.Button(self.ventana1, text="Recuperar", command=self.recuperar)
-		self.boton1.grid(column=0, row=2, pady=10)
-		self.scrolledtext1=st.ScrolledText(self.ventana1, width=100, height=30)
-		self.scrolledtext1.grid(column=0,row=3, padx=10, pady=10)
-		self.ventana1.mainloop()
+    def __init__(self):
+        self.ventana1 = tk.Tk()
+        self.label1 = ttk.Label(text="Ingrese URL del sitio web:")
+        self.label1.grid(column=0, row=0, pady=10)
+        self.dato1 = tk.StringVar()
+        self.entry1 = ttk.Entry(self.ventana1, width=100, textvariable=self.dato1)
+        self.entry1.grid(column=0, row=1, pady=10)
+        self.boton1 = ttk.Button(
+            self.ventana1, text="Recuperar", command=self.recuperar
+        )
+        self.boton1.grid(column=0, row=2, pady=10)
+        self.scrolledtext1 = st.ScrolledText(self.ventana1, width=100, height=30)
+        self.scrolledtext1.grid(column=0, row=3, padx=10, pady=10)
+        self.ventana1.mainloop()
 
-	def recuperar(self):
-		try:
-			pagina=request.urlopen(self.dato1.get())
-			datos=pagina.read().decode("utf-8")
-			self.scrolledtext1.delete(1.0,tk.END)
-			self.scrolledtext1.insert(tk.INSERT,datos)
-		except error.HTTPError as err:
-			mb.showinfo("Problemas", "No se puede aceder a dicho recurso")
+    def recuperar(self):
+        try:
+            pagina = request.urlopen(self.dato1.get())
+            datos = pagina.read().decode("utf-8")
+            self.scrolledtext1.delete(1.0, tk.END)
+            self.scrolledtext1.insert(tk.INSERT, datos)
+        except error.HTTPError as err:
+            mb.showinfo("Problemas", "No se puede aceder a dicho recurso")
 
 
-aplicacion1=Aplicacion()
+aplicacion1 = Aplicacion()
