6c465c3802dc082464d100ef4765ba4bc1fe8a35
David Neumann <86894039+davidneumann01@users.noreply.github.com>
2022-11-30 22:40:27 -0300

    Add files via upload

--- proyect4/articulos.py
+++ proyect4/articulos.py
@@ -1,25 +1,24 @@
 import sqlite3
 
-class Articulos:
 
+class Articulos:
     def abrir(self):
-        conexion=sqlite3.connect("bd1.db")
+        conexion = sqlite3.connect("bd1.db")
         return conexion
 
-
     def alta(self, datos):
-        cone=self.abrir()
-        cursor=cone.cursor()
-        sql="insert into articulos(descripcion, precio) values (?,?)"
+        cone = self.abrir()
+        cursor = cone.cursor()
+        sql = "insert into articulos(descripcion, precio) values (?,?)"
         cursor.execute(sql, datos)
         cone.commit()
         cone.close()
 
     def consulta(self, datos):
         try:
-            cone=self.abrir()
-            cursor=cone.cursor()
-            sql="select descripcion, precio from articulos where codigo=?"
+            cone = self.abrir()
+            cursor = cone.cursor()
+            sql = "select descripcion, precio from articulos where codigo=?"
             cursor.execute(sql, datos)
             return cursor.fetchall()
         finally:
@@ -27,32 +26,32 @@ class Articulos:
 
     def recuperar_todos(self):
         try:
-            cone=self.abrir()
-            cursor=cone.cursor()
-            sql="select codigo, descripcion, precio from articulos"
+            cone = self.abrir()
+            cursor = cone.cursor()
+            sql = "select codigo, descripcion, precio from articulos"
             cursor.execute(sql)
             return cursor.fetchall()
         finally:
             cone.close()
-            
+
     def baja(self, datos):
         try:
-            cone=self.abrir()
-            cursor=cone.cursor()
-            sql="delete from articulos where codigo=?"
+            cone = self.abrir()
+            cursor = cone.cursor()
+            sql = "delete from articulos where codigo=?"
             cursor.execute(sql, datos)
             cone.commit()
-            return cursor.rowcount # retornamos la cantidad de filas borradas
+            return cursor.rowcount  # retornamos la cantidad de filas borradas
         except:
             cone.close()
-    
+
     def modificacion(self, datos):
         try:
-            cone=self.abrir()
-            cursor=cone.cursor()
-            sql="update articulos set descripcion=?, precio=? where codigo=?"
+            cone = self.abrir()
+            cursor = cone.cursor()
+            sql = "update articulos set descripcion=?, precio=? where codigo=?"
             cursor.execute(sql, datos)
             cone.commit()
-            return cursor.rowcount # retornamos la cantidad de filas modificadas            
+            return cursor.rowcount  # retornamos la cantidad de filas modificadas
         except:
             cone.close()
